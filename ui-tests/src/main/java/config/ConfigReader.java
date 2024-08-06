4b07d0adcbf6bf68aa1eeb58f277b6986227eb97
Andrew Evdokimenko <evdokimenko_9797@mail.ru>
2024-08-06 17:39:14 +0300

    fix: Изменено на try-with-resources

--- ui-tests/src/main/java/config/ConfigReader.java
+++ ui-tests/src/main/java/config/ConfigReader.java
@@ -2,14 +2,15 @@ package config;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class ConfigReader {
     private static final Properties properties = new Properties();
 
     public synchronized static String getProperty(String key) {
-        try {
-            properties.load(new FileInputStream("src\\test\\resources\\config.properties"));
+        try (InputStream inputStream = new FileInputStream("src\\test\\resources\\config.properties")) {
+            properties.load(inputStream);
         } catch (IOException e) {
             e.printStackTrace();
         }
